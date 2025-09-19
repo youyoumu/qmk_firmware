765222690ecae990b8928dad87a547c87738079e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-09-19 18:57:31 +0700

    Setup miryoku

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -15,7 +15,7 @@
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 150
 
 // Auto Shift
 #define NO_AUTO_SHIFT_ALPHA
--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,4 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+AUTO_SHIFT_ENABLE = no
